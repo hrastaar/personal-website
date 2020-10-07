cd2873505e62b0bf0e5e9d26862d7dc0020e8078
Rastaar Haghi <rhaghi@usc.edu>
2020-10-06 17:25:12 -0700

    allowed error handling for json data fetching

--- app.js
+++ app.js
@@ -34,95 +34,39 @@ app.listen(80, () => {
 // season example 19-20
 app.get('/api/premierleague/:season/standings', (req, res) => {
 
-    fs.readFile('./json/' + req.params.season + '/PremierLeagueTable.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
+    var fileName = './json/' + req.params.season + '/PremierLeagueTable.json';
+    fs.access(fileName, fs.constants.R_OK, (error) => {
+      if(!error) {
+        fs.readFile('./json/' + req.params.season + '/PremierLeagueTable.json', (err, json) => {
+          let obj = JSON.parse(json);
+          obj["status"] = 0;
+          res.json(obj);
+        });
+      } else {
+        console.log(error);
+        res.send({status: -1});
+      }  
     });
+    
 });
 
 app.get('/api/premierleague/:season/week/:weekNumber', (req, res) => {
     var fileName = './json/' + req.params.season +  '/Week' + req.params.weekNumber + '.json';
-    console.log(fileName);
-    fs.readFile(fileName, (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    });
-});
 
-// post request for when user presses donate button on locahost:3000/
-app.get('/donate', (req, res) => {
-    const create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
-        },
-        "redirect_urls": {
-            "return_url": "http://localhost:80/success",
-            "cancel_url": "http://localhost:80/cancelled"
-        },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "5 Dollar Donation",
-                    "sku": "001",
-                    "price": "5.00",
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": "5.00"
-            },
-            "description": "Donate to the developers of this app"
-        }]
-    }
-
-    paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-            throw error;
-        } else {
-            for(let i = 0;i < payment.links.length;i++){
-                if(payment.links[i].rel === 'approval_url'){
-                res.redirect(payment.links[i].href);
-                }
-            }
-        }
-    });
-});
-
-
-// set up sandbox paypal sdk with my business account
-paypal.configure({
-    'mode': 'sandbox', // OPTIONS: sandbox, or live
-    'client_id': 'AV5n0ECAd0lFV-ane6nfVt1oPCBt97oaJQP8RYzrEq3YdE6G9hAmspLK-d7CujbOXgIf80FpGXAdgIIu',
-    'client_secret': 'EDaHfl0W72tmdKNuf3UHWM6hc2iix69LMsDruTYJaSzPLNwhCi5rW0pnoCq7QOdwAwHRHatBCmCVtR2G'
-});
-
-  
-app.get('/success', (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-  
-    const execute_payment_json = {
-      "payer_id": payerId,
-      "transactions": [{
-          "amount": {
-              "currency": "USD",
-              "total": "5.00"
-          }
-      }]
-    };
-  
-    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          throw error;
+    fs.access(fileName, fs.constants.R_OK, (error) => {
+      if(!error) {
+        console.log(fileName);
+        fs.readFile(fileName, (err, json) => {
+            let obj = JSON.parse(json);
+            obj["status"] = 0;
+            res.json(obj);
+        });
       } else {
-          console.log(JSON.stringify(payment));
-          res.render('success');
+        console.log("Couldn't find data");
+        res.send({status: -1});
       }
-  });
-  });
+    });
+    console.log("Page visited");
+});
   
   app.get('/cancelled', (req, res) => res.render('projects'));
